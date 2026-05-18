b1639c71898c89e544f2880b61f717bbf8791ef2
Jonathan <endzs200@gmail.com>
2026-05-18 11:57:50 -0300

    Estrutura controllers e rotas base da API (faltando pacientecontroller) AB#41

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,9 +1,13 @@
 <?php
-<<<<<<< HEAD
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AtendimentoController;
 use App\Http\Controllers\AuthController;
 
 Route::get('/login', [AuthController::class, 'index']);
 Route::get('/logout', [AuthController::class, 'logout']);
+Route::get('/painel', [AtendimentoController::class, 'painel'])->name('painel');
+Route::get('/chamadas/historico', [AtendimentoController::class, 'historico'])->name('chamadas.historico');
+Route::post('/atendimento/finalizar/{id}', [AtendimentoController::class, 'finalizar'])->name('atendimento.finalizar');
 
 Route::get('/dashboard', function () {
     if (!session()->has('medico_id')) {
@@ -12,17 +16,8 @@ Route::get('/dashboard', function () {
 
     return 'Dashboard do médico';
 });
-=======
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AtendimentoController;
 
 Route::get('/teste', function () {
     return 'FUNCIONOU';
 });
 
-Route::get('/painel', [AtendimentoController::class, 'painel'])->name('painel');
-
-Route::get('/chamadas/historico', [AtendimentoController::class, 'historico'])->name('chamadas.historico');
-
-Route::post('/atendimento/finalizar/{id}', [AtendimentoController::class, 'finalizar'])->name('atendimento.finalizar');
->>>>>>> 5616b50 (Commit AB#38)
